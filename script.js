0daa335ca2a9b5da3659ad00c2c9451e05aee403
pphelps28 <p.w.phelps28@gmail.com>
2020-04-01 19:36:00 -0400

    lab prep

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-//unique config variable
 const firebaseConfig = {
-   //your config here
+    apiKey: "AIzaSyAio6vwdZAJ1GlzX7C0Mg8bR6gLt1EpVBQ",
+    authDomain: "fir-practice-87288.firebaseapp.com",
+    databaseURL: "https://fir-practice-87288.firebaseio.com",
+    projectId: "fir-practice-87288",
+    storageBucket: "fir-practice-87288.appspot.com",
+    messagingSenderId: "270975170469",
+    appId: "1:270975170469:web:cef00b31317d4d81b61a4c",
+    measurementId: "G-R28BVC59V8"
 };
+
 firebase.initializeApp(firebaseConfig)
 const auth = firebase.auth()
 
@@ -10,7 +17,7 @@ document.getElementById("btnSignUp").addEventListener('click', e => {
     console.log('clicked')
     const email = document.getElementById("txtEmail").value;
     const pass = document.getElementById("txtPassword").value;
-    firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function (error) {
+    auth.createUserWithEmailAndPassword(email, pass).catch(function (error) {
         console.log(error.message);
     });
 })
@@ -19,20 +26,23 @@ document.getElementById("btnLogin").addEventListener('click', e => {
     console.log('clicked')
     const email = document.getElementById("txtEmail").value;
     const pass = document.getElementById("txtPassword").value;
-    const promise = firebase.auth().signInWithEmailAndPassword(email, pass);
-    promise.catch(e => { console.log(e.massage) })
+    auth.signInWithEmailAndPassword(email, pass).catch((error) => {
+        console.log(error.message)
+    })
 })
 //logout
 document.getElementById("btnLogOut").addEventListener('click', e => {
-    firebase.auth().signOut()
+    auth.signOut()
     console.log('logged out')
 })
-
+//authorize user
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
         console.log(user)
         document.getElementById("btnLogOut").classList.remove('hide')
+        alert('logged in!')
     } else {
         document.getElementById("btnLogOut").classList.add('hide')
+        alert('logged out!')
     }
 })
